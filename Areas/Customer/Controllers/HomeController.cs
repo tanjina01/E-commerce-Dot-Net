c356bbeaee39d6d7e1c8b3f3bf505d4312c64465
tanjina01 <tanjinatonny3@gmail.com>
2024-09-03 01:48:28 +0600

    Initial Commit

---
+++ Areas/Customer/Controllers/HomeController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StataIT.Data;
+using StataIT.Models;
+using StataIT.Utility;
+using X.PagedList;
+
+namespace StataIT.Controllers
+{
+    [Area("Customer")]
+
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        public HomeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index(int? page)
+        {
+            return View(_db.Products.Include(c => c.ProductTypes).ToList().ToPagedList(page ?? 1, 9));
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        //GET product detail acation method
+
+        public ActionResult Detail(int? id)
+        {
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = _db.Products.Include(c => c.ProductTypes).FirstOrDefault(c => c.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        //POST product detail action method
+        [HttpPost]
+        [ActionName("Detail")]
+        public ActionResult ProductDetail(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = _db.Products.Include(c => c.ProductTypes).FirstOrDefault(c => c.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            List<Products> products = HttpContext.Session.Get<List<Products>>("products") ?? new List<Products>();
+            products.Add(product);
+            HttpContext.Session.Set("products", products);
+
+            return RedirectToAction(nameof(Detail));
+        }
+
+        //GET Remove action Method
+        [ActionName("Remove")]
+        public IActionResult RemoveToCart(int? id)
+        {
+            List<Products> products = HttpContext.Session.Get<List<Products>>("products");
+            if (products != null)
+            {
+                var product = products.FirstOrDefault(c => c.Id == id);
+                if (product != null)
+                {
+                    products.Remove(product);
+                    HttpContext.Session.Set("products", products);
+                }
+            }
+            return RedirectToAction(nameof(Cart));
+        }
+        [HttpPost]
+        public IActionResult Remove(int? id)
+        {
+            List<Products> products = HttpContext.Session.Get<List<Products>>("products");
+            if(products!=null)
+            {
+                var product = products.FirstOrDefault(c => c.Id == id);
+                if(product!=null)
+                {
+                    products.Remove(product);
+                    HttpContext.Session.Set("products", products);
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        //GET product Cart action method
+        public IActionResult Cart()
+        {
+            List<Products> products = HttpContext.Session.Get<List<Products>>("products");
+            if(products==null)
+            {
+                products = new List<Products>();
+            }
+            return View(products);
+        }
+    }
+}
---
+++ Areas/Customer/Controllers/OrderController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StataIT.Data;
+using StataIT.Models;
+using StataIT.Utility;
+
+namespace StataIT.Areas.Customer.Controllers
+{
+
+    [Area("Customer")]
+    public class OrderController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        public OrderController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //GET Checkout actioin method
+
+        public IActionResult Checkout()
+        {
+            var order = new Order
+            {
+                OrderDate = DateTime.Now // Set default order date to current date
+            };
+            return View(order);
+        }
+
+
+        // POST Checkout action method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Checkout(Order anOrder)
+        {
+            if (ModelState.IsValid)
+            {
+                List<Products> products = HttpContext.Session.Get<List<Products>>("products");
+                if (products != null)
+                {
+                    foreach (var product in products)
+                    {
+                        OrderDetails orderDetails = new OrderDetails
+                        {
+                            PorductId = product.Id
+                        };
+                        anOrder.OrderDetails.Add(orderDetails);
+                    }
+                }
+
+                anOrder.OrderNo = GetOrderNo();
+                _db.Orders.Add(anOrder);
+                await _db.SaveChangesAsync();
+                HttpContext.Session.Set("products", new List<Products>());
+
+                return RedirectToAction("OrderConfirmation", new { id = anOrder.Id });
+            }
+            return View(anOrder);
+        }
+
+        public string GetOrderNo()
+        {
+            int rowCount = _db.Orders.ToList().Count() + 1;
+            return rowCount.ToString("000");
+        }
+
+        public IActionResult OrderConfirmation(int id)
+        {
+            var order = _db.Orders.FirstOrDefault(o => o.Id == id);
+            return View(order);
+        }
+    }
+}
